c4960fabfdfd9561bd6fb1dd83c0e36ba8b140ba
Bhaven Kakade <thisisbhaven@gmail.com>
2020-06-25 18:28:37 +0530

    blog body

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../components/Layout"
 
 export const query = graphql`
@@ -8,6 +8,9 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
@@ -17,6 +20,7 @@ const Blog = props => {
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
     </Layout>
   )
 }
